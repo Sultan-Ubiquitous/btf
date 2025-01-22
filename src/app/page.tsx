ad175f0bb7e23c15708d293fef84bc62129cde99
Sultan-Ubiquitous <azimakhorajiya@gmail.com>
2025-01-22 23:23:02 +0530

    aight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center bg-black">
-     <p className="text-8xl font-extrabold text-white">Builder Thinker Fighter</p>
+      <p className="text-center text-white font-extrabold text-4xl sm:text-6xl md:text-8xl">
+        Builder Thinker Fighter
+      </p>
     </div>
   );
 }
