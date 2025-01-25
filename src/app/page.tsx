2de81d5f34f51e220fc5462a40406a0dfc193e5c
Sultan-Ubiquitous <azimakhorajiya@gmail.com>
2025-01-25 10:17:30 +0530

    belh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center bg-black">
       <p className="text-center text-white font-extrabold text-4xl sm:text-6xl md:text-8xl">
-        Builder Thinker Fighter
+        Builder Fighter Thinker
       </p>
     </div>
   );
